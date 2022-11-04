b54e5aecb9791a22ae431dbe1486ff24d17ed829
Roberto A Moraes <roberto.moraes@stilingue.com.br>
2022-11-04 10:38:06 -0500

    subindo a aplicação

---
+++ src/test/java/leilao/helloWord.java
@@ -0,0 +1,19 @@
+package leilao;
+
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Leilao;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class helloWord {
+
+    @Test
+    public void hello() {
+        LeilaoDao mock = Mockito.mock(LeilaoDao.class);
+        List<Leilao> todos = mock.buscarTodos();
+        Assert.assertTrue(todos.isEmpty());
+    }
+}
